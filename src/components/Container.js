172866bc5655c535164021b1f986e987e2bbb730
Stephen Chesnowitz <steve@margoscargo.com>
2023-09-11 03:19:43 -0400

    snippet and Links

---
+++ src/components/Container.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react"
+
+function Container() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Container
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Header from "./components/Header.js"
+import HomeGuest from './components/HomeGuest.js';
+import Footer from './components/Footer.js';
+import About from './components/About.js';
+import Terms from './components/Terms.js';
+function Main() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={< HomeGuest/>} />
+        <Route path='/about' element={< About/>} />
+        <Route path='/terms' element={< Terms/>} />
+        </Routes>  
+      <Footer />
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+  
+    </BrowserRouter>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Main />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

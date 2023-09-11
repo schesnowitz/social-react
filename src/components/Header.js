0b456c0056b3c1f06008893cd39f6b692b901117
Stephen Chesnowitz <steve@margoscargo.com>
2023-09-11 17:51:51 -0400

    conditional homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import {Link} from 'react-router-dom'
 import HeaderLoggedOut from "./HeaderLoggedOut"
 import HeaderLoggedIn from "./HeaderLoggedIn"
 
-function Header() {
-  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('halcyonToken')))
+function Header(props) {
+  
   return(
     <header className="header-bar bg-primary mb-3">
     <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -13,7 +13,7 @@ function Header() {
           Halcyon Freight
         </Link> 
       </h4>
-     {loggedIn ? <HeaderLoggedIn setLoggedIn={setLoggedIn} /> : <HeaderLoggedOut setLoggedIn={setLoggedIn} />}
+     {props.loggedIn ? <HeaderLoggedIn setLoggedIn={props.setLoggedIn} /> : <HeaderLoggedOut setLoggedIn={props.setLoggedIn} />}
     </div>
   </header>
   )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from "./components/Header.js"
@@ -8,12 +8,14 @@ import HomeGuest from './components/HomeGuest.js';
 import Footer from './components/Footer.js';
 import About from './components/About.js';
 import Terms from './components/Terms.js';
+import Home from './components/Home.js';
 function Main() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('halcyonToken')))
   return (
     <BrowserRouter>
-      <Header />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
-        <Route path='/' element={< HomeGuest/>} />
+        <Route path='/' element={loggedIn ? < Home/> : <HomeGuest />} />
         <Route path='/about' element={< About/>} />
         <Route path='/terms' element={< Terms/>} />
         </Routes>  
